4f02a7229c0ef2d3320decb9cfc41a075e16697f
Alon Cohen <alon@placer.io>
2022-03-15 12:05:52 +0200

    Update get_hn_data.py
    get_hn_data - updaes

--- get_hn_data.py
+++ get_hn_data.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 from bs2json import bs2json
 import pandas as pd
+from typing import Tuple
+
 
 import logging
 
@@ -12,132 +14,120 @@ HN_HOME_PATH = 'https://news.ycombinator.com/news'
 HN_API_PATH_TOP_STORIES = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy="$priority"&limitToFirst=%s'
 HN_API_PATH_ITEM = 'https://hacker-news.firebaseio.com/v0/item/'
 
-
 GRAVITY = 1.8
 
 
 class ApiError(Exception):
-  pass
+    pass
 
 
 class ScrapingError(Exception):
-  pass
+    pass
 
 
-def get_records_metadata(article_metadata):
-
-  """
+def get_records_metadata(article_metadata: list) -> Tuple[str, str, str]:
+    """
   extract items from nested dict
   :param article_metadata:
   :return:
   """
 
-  article_metadata = sorted(article_metadata, key=lambda k: k.keys(), reverse=True)  # sort dict list
-  rank = article_metadata[0].get('span').get('text')
-  title = article_metadata[2].get('a').get('text')
-  article_link = article_metadata[2].get('a').get('attributes').get('href')
-  return rank, title, article_link
+    article_metadata = sorted(article_metadata, key=lambda k: k.keys(), reverse=True)  # sort dict list
+    rank = article_metadata[0].get('span').get('text')
+    title = article_metadata[2].get('a').get('text')
+    article_link = article_metadata[2].get('a').get('attributes').get('href')
+    return rank, title, article_link
 
 
-def scrape_hn_news(p=1):
+def scrape_hn_news(p: int = 1) -> pd.DataFrame:
+    logging.info(f'scrape_hn_news page: {p} - START')
 
-  """
-  this function will scrape
-  :param p: news site page
-  :return:
-  """
+    hn_homepage = requests.get(f"{HN_HOME_PATH}?p={p}")
 
-  logging.info(f'scrape_hn_news page: {p} - START')
+    if hn_homepage.status_code != 200:
+        raise ScrapingError(f"get request failed, reason - {hn_homepage.reason}")
 
-  hn_homepage = requests.get(f"{HN_HOME_PATH}?p={p}")
+    soup = BeautifulSoup(hn_homepage.content, "html.parser")  # parse HTML page
 
-  if hn_homepage.status_code != 200:
-    raise ScrapingError(f"get request failed, reason - {hn_homepage.reason}")
+    converter = bs2json()  # initiate converter object
+    hack_news = converter.convertAll(soup.find_all("tr", class_='athing'))
 
-  soup = BeautifulSoup(hn_homepage.content, "html.parser")  # parse HTML page
+    hn_df = pd.DataFrame(i['tr'] for i in hack_news)
+    ids_df = hn_df.apply(lambda x: x['attributes'].get('id'), axis=1)  # extract ids from dict
 
-  converter = bs2json()  # initiate converter object
-  hack_news = converter.convertAll(soup.find_all("tr", class_='athing'))
+    hn_df = hn_df.apply(lambda x: pd.Series(x['td']), axis=1)
 
-  hn_df = pd.DataFrame(i['tr'] for i in hack_news)
-  ids_df = hn_df.apply(lambda x: x['attributes'].get('id'), axis=1)  # extract ids from dict
+    hn_df[['rank', 'title', 'link']] = hn_df.apply(lambda x: pd.Series(get_records_metadata(x)), axis=1)
+    hn_df['id'] = ids_df
+    hn_df = hn_df[['rank', 'id', 'title', 'link']]
 
-  hn_df = hn_df.apply(lambda x: pd.Series(x['td']), axis=1)
+    logging.info(f'scrape_hn_news page: {p} - END')
 
-  hn_df[['rank', 'title', 'link']] = hn_df.apply(lambda x: pd.Series(get_records_metadata(x)), axis=1)
-  hn_df['id'] = ids_df
-  hn_df = hn_df[['rank', 'id', 'title', 'link']]
-
-  logging.info(f'scrape_hn_news page: {p} - END')
-
-  return hn_df
+    return hn_df
 
 
-def calc_rank(article):
-
-  """
+def calc_rank(article: pd.Series) -> float:
+    """
   function that rank article based on comments, time since publication and gravity constant (1.8)
   Rank = (P-1) / (T+2)^G
   :param article: pd rows
   :return:
   """
 
-  return (article['descendants'] - 1) / (article['time_since_sub'] + 2) ** GRAVITY
+    return (article['descendants'] - 1) / (article['time_since_sub'] + 2) ** GRAVITY
 
 
-def hn_news_api(stories_limit=40):
+def hn_news_api(stories_limit: int = 40) -> pd.DataFrame:
+    """
 
-  """
+    note - can be speed-up using multi threading
+    :param stories_limit:
+    :return:
+    """
+    logging.info(f'hn_news_api - START')
 
-  note - can be speed-up using multi threading
-  :param stories_limit:
-  :return:
-  """
+    hn_request = requests.get(HN_API_PATH_TOP_STORIES % stories_limit, timeout=10)  # send request to hacker news AIP
+    if hn_request.status_code != 200:
+        raise ApiError(f"api request failed, reason - {hn_request.reason}")
 
-  logging.info(f'hn_news_api - START')
+    top_articles = eval(hn_request.text)  # convert content (str) to list
 
-  hn_request = requests.get(HN_API_PATH_TOP_STORIES % stories_limit, timeout=10)  # send request to hacker news AIP
-  if hn_request.status_code != 200:
-    raise ApiError(f"api request failed, reason - {hn_request.reason}")
+    logging.info(f'API query completed, received {len(top_articles)} articles!')
 
-  top_articles = eval(hn_request.text)  # convert content (str) to list
+    now = datetime.now()  # calc current time
 
-  logging.info(f'API query completed, received {len(top_articles)} articles!')
+    articles_list = []
 
-  now = datetime.now()  # calc current time
+    for index, article_id in enumerate(top_articles):
 
-  articles_list = []
+        try:
+            article_request = requests.get(f'{HN_API_PATH_ITEM}/{str(article_id)}.json')
+            if article_request.status_code != 200:
+                raise ApiError(f"api request failed, reason - {article_request.reason}")
+            article_info = eval(article_request.content)  # convert content to list
+            articles_list.append(article_info)
 
-  for index, article_id in enumerate(top_articles):
+            if index % 10 == 0 and index != 0:
+                logging.info(f'{index} articles processed')
 
-    try:
-      article_request = requests.get(f'{HN_API_PATH_ITEM}/{str(article_id)}.json')
-      if article_request.status_code != 200:
-        raise ApiError(f"api request failed, reason - {article_request.reason}")
-      article_info = eval(article_request.content)  # convert content to list
-      articles_list.append(article_info)
+        except Exception as e:
+            logging.info(e)
 
-      if index % 10 == 0 and index != 0:
-        logging.info(f'{index} articles processed')
+    hn_df = pd.DataFrame(articles_list)
+    hn_df['timestamp'] = pd.to_datetime(hn_df['time'], unit='s')  # convert to timestamp
+    hn_df['time_since_sub'] = hn_df.apply(lambda x: (now - x['timestamp']).total_seconds() / 3600,
+                                          axis=1)  # calc time since publish
+    hn_df['rank'] = hn_df.apply(lambda x: calc_rank(x), axis=1)  # add rank column
+    hn_df = hn_df.sort_values('rank', ascending=False)  # sort by rank
 
-    except Exception as e:
-      logging.info(e)
+    hn_df = hn_df[['id', 'title', 'url', 'rank']]
 
-  hn_df = pd.DataFrame(articles_list)
-  hn_df['timestamp'] = pd.to_datetime(hn_df['time'], unit='s')  # convert to timestamp
-  hn_df['time_since_sub'] = hn_df.apply(lambda x: (now - x['timestamp']).total_seconds() / 3600, axis=1)  # calc time since publish
-  hn_df['rank'] = hn_df.apply(lambda x: calc_rank(x), axis=1)  # add rank column
-  hn_df = hn_df.sort_values('rank', ascending=False)  # sort by rank
+    logging.info(f'hn_news_api - END')
 
-  hn_df = hn_df[['id', 'title', 'url', 'rank']]
-
-  logging.info(f'hn_news_api - END')
-
-  return hn_df
+    return hn_df
 
 
 def main(method: str = "api") -> pd.DataFrame:
-
     """
 
     This code will return a data frame of top 40 articles published by Hacker News, ranked by their popularity
@@ -152,12 +142,12 @@ def main(method: str = "api") -> pd.DataFrame:
     assert method in ['api', "scraping"], f"Oops, method must be 'api' OR 'scraping', received %s {method}"
 
     if method == 'scraping':
-      df1 = scrape_hn_news(p=1)  # scrape page 1
-      df2 = scrape_hn_news(p=2)  # scrape page 2
-      hn_df = pd.concat([df1, df2])[0:40]  # filter to top 40
+        df1 = scrape_hn_news(p=1)  # scrape page 1
+        df2 = scrape_hn_news(p=2)  # scrape page 2
+        hn_df = pd.concat([df1, df2])[0:40]  # filter to top 40
 
     else:
-      hn_df = hn_news_api()
+        hn_df = hn_news_api()
 
     return hn_df
 
